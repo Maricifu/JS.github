dfc5864215396ab65a2a05509ffbad15412d55ae
Maricifu <152333294+Maricifu@users.noreply.github.com>
2024-02-22 16:29:26 -0500

    cargarClientes

---
+++ Billing-proyect/js/client.js
@@ -0,0 +1,14 @@
+const listaClientes= [];
+
+const cargarClientes=()=>{
+    for(let i=0;i<=10;i++){
+        const nuevoCliente={
+            id:i,
+            nombre:faker.name.findName(),
+            edad: Math.floor(Math.random*30)+18,
+            email: faker.internet.email()
+        };
+        listaClientes.push(nuevoCliente);
+    }
+}
+
