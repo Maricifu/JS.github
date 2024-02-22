6d8b04744d8a5306fd19adf695d500f6bcc8035b
Maricifu <152333294+Maricifu@users.noreply.github.com>
2024-02-22 16:38:51 -0500

    cargarFormularioCientes

--- Billing-proyect/js/client.js
+++ Billing-proyect/js/client.js
@@ -12,3 +12,22 @@ const cargarClientes=()=>{
     }
 }
 
+const cargarFormularioClientes=()=>{
+    const clientesForm= document.getElementById('clientes-form');
+    clientesForm.innerHTML = `
+        <form>
+            <label for="nombreCliente">Nombre del Cliente:</label>
+            <input type="text" id="nombreCliente" required>
+            <label for="edadCliente">Edad del Cliente:</label>
+            <input type="number" id="edadCliente" required>
+            <label for="emailCliente">Correo Electrónico del Cliente:</label>
+            <input type="email" id="emailCliente" required>
+            <button type="button" onclick="crearCliente()">Crear Cliente</button>
+            <button type="button" onclick="mostrarListado()">Ver Listado de Clientes</button>
+            <!-- Aquí se puede añadir más funcionalidad, como modificar y eliminar clientes -->
+        </form>
+    `;
+    const listadoClientes = document.getElementById('listado-clientes');
+    listaClientes.style.display='none';
+}
+
