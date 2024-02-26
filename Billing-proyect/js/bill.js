92d1ee788643d68cf322e3d0e49ce933e3725b06
Maricifu <152333294+Maricifu@users.noreply.github.com>
2024-02-26 17:08:05 -0500

    cargarFormularioFacturas

--- Billing-proyect/js/bill.js
+++ Billing-proyect/js/bill.js
@@ -13,3 +13,38 @@ const actulizarProductosEnFacturas=()=>{
     const opcionesProductos=generarOptionsProductos();
     productosSelect.innerHTML=opcionesProductos;
 }
+
+const cargarFormularioFacturas=()=>{
+    const facturasForm=document.getElementById('facturas-form');
+    facturasForm.innerHTML = `
+        <form>
+            <label for="fechaFactura">Fecha de la Factura:</label>
+            <input type="date" id="fechaFactura" required>
+            
+            <label for="clienteFactura">Cliente:</label>
+            <select id="clienteFactura" required>
+                ${generarOptionsClientes()}
+            </select>
+
+            <label for="productosFactura">Productos:</label>
+            <select id="productosFactura" multiple required>
+                ${generarOptionsProductos()}
+            </select>
+
+            <label for="cantidadProducto">Cantidad:</label>
+            <input type="number" id="cantidadProducto" required>
+
+            <button type="button" onclick="agregarItemFactura()">Agregar Item</button>
+
+            <h3>Items de la Factura:</h3>
+            <ul id="listado-items"></ul>
+
+            <button type="button" onclick="crearFactura()">Crear Factura</button>
+            <button type="button" onclick="mostrarListadoFacturas()">Ver Listado de Facturas</button>
+        </form>
+    `;
+
+    const listaFacturas=document.getElementById('listado-facturas');
+    listaFacturas.style.display='none';
+
+}
